7afec5093f702dc68977a2d124e29c359f4eade4
rakib-utsho <rakibutsho1920@gmail.com>
2024-11-21 05:23:22 +0600

    matching Sequence

--- JS/app.js
+++ JS/app.js
@@ -35,24 +35,40 @@ function userFlash(btn){
 };
 
 function levelUp(){
+    userSeq = [];
     level++;
     h3.innerText = `Level ${level}`;
 
     // random btn choose
-    let randomIdx = Math.floor(Math.random() * 3);
+    let randomIdx = Math.floor(Math.random() * 4);
     let randomColor = btns[randomIdx];
     let randomBtn = document.querySelector(`.${randomColor}`);
-    console.log(randomIdx);
-    console.log(randomColor);
-    console.log(randomBtn);
-
+    // console.log(randomIdx);
+    // console.log(randomColor);
+    // console.log(randomBtn);
+    gameSeq.push(randomColor);
+    console.log(gameSeq);
     gameFlash(randomBtn);
 };
+
+function checkAns (idx) {
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+    }else{
+        h3.innerText = `Game Over! Press any key to restart`;
+    }
+}
 
 function btnPress () {
     let btn = this;
     console.log(btn);
     userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
 };
 
 let allBtns = document.querySelectorAll(".btn");

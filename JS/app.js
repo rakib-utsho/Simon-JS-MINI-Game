317411cffce7fd325ae4f4a618a7fb5286786204
rakib-utsho <rakibutsho1920@gmail.com>
2024-11-21 04:54:04 +0600

    button event listeners

---
+++ JS/app.js
@@ -0,0 +1,61 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "purple", "green"];
+
+let started = false;
+let level = 0;
+
+let h3 = document.querySelector("h3");
+
+// initial level setup;
+
+
+document.addEventListener("keypress", function(){
+    if(started == false){
+        console.log("game is started");
+        started = true;
+    
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove("flash")
+    }, 250);
+};
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function() {
+        btn.classList.remove("userflash")
+    }, 250);
+};
+
+function levelUp(){
+    level++;
+    h3.innerText = `Level ${level}`;
+
+    // random btn choose
+    let randomIdx = Math.floor(Math.random() * 3);
+    let randomColor = btns[randomIdx];
+    let randomBtn = document.querySelector(`.${randomColor}`);
+    console.log(randomIdx);
+    console.log(randomColor);
+    console.log(randomBtn);
+
+    gameFlash(randomBtn);
+};
+
+function btnPress () {
+    let btn = this;
+    console.log(btn);
+    userFlash(btn);
+};
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress)
+};
